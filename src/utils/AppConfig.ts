530b8dd36a0ba11c32bfd7e11c9320f1e3881b3b
John Crespi Oncher <johnoncher@gmail.com>
2022-09-19 11:49:35 -0400

    fix(image): updated favicon

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'John Oncher',
+  title: 'John Oncher',
+  description: 'John Oncher Portfolio Site',
+  author: 'John Oncher',
   locale_region: 'en-us',
   locale: 'en',
 };
